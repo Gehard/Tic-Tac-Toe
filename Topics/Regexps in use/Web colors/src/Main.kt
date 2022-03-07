6b257b153f28b71e25f1dfc98c4f47caab635730
gehard <gehardnjoro@gmail.com>
2022-03-07 21:02:22 +0300

    initial

--- Topics/Regexps in use/Web colors/src/Main.kt
+++ Topics/Regexps in use/Web colors/src/Main.kt
@@ -1,5 +1,7 @@
 fun main() {
-    val text = readLine()!!
+    val text = readln()
     val regexColors = "#[0-9a-fA-F]{6}\\b".toRegex()
-    // write your code here
+
+    val matchResult = regexColors.findAll(text)
+    for (matches in matchResult) println(matches.value)
 }
