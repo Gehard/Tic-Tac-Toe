f5da6d548aee7f4a8575a626a2e9b97b85d23d7f
gehard <gehardnjoro@gmail.com>
2022-03-15 15:37:51 +0300

    initial

--- Simple Tic-Tac-Toe/task/src/tictactoe/Main.kt
+++ Simple Tic-Tac-Toe/task/src/tictactoe/Main.kt
@@ -1,127 +1,47 @@
 package tictactoe
-
-fun printBoard(cells: String) {
-    println("---------")
-    println("| ${cells[0]} ${cells[1]} ${cells[2]} |")
-    println("| ${cells[3]} ${cells[4]} ${cells[5]} |")
-    println("| ${cells[6]} ${cells[7]} ${cells[8]} |")
-    println("---------")
-}
-
-fun getRow(cells: String, row: Int): String {
-    return "${cells[row * 3]}${cells[row * 3 + 1]}${cells[row * 3 + 2]}"
-}
-
-fun getCol(cells: String, col: Int): String {
-    return "${cells[col]}${cells[col + 3]}${cells[col + 6]}"
-}
-
-// if one row is owned by one player
-// we have to check that the other
-// two rows are NOT owned by the other player
-fun checkRows(cells: String): String? {
-    for (row in 0..2) {
-        if (getRow(cells, row) == "XXX") {
-            for (otherRow in 0..2) {
-                if (row != otherRow) {
-                    if (getRow(cells, otherRow) == "OOO") {
-                        return "Impossible"
-                    }
-                }
-            }
-            return "X wins"
-        }
-        else if (getRow(cells, row) == "OOO") {
-            for (otherRow in 0..2) {
-                if (row != otherRow) {
-                    if (getRow(cells, otherRow) == "XXX") {
-                        return "Impossible"
-                    }
-                }
-            }
-            return "O wins"
-        }
-    }
-    return null
-}
-
-// if one col is owned by one player
-// we have to check that the other
-// two cols are NOT owned by the other player
-fun checkColumns(cells: String): String? {
-    for (col in 0..2) {
-        if (getCol(cells, col) == "XXX") {
-            for (otherCol in 0..2) {
-                if (col != otherCol) {
-                    if (getCol(cells, otherCol) == "OOO") {
-                        return "Impossible"
-                    }
-                }
-            }
-            return "X wins"
-        }
-        else if (getCol(cells, col) == "OOO") {
-            for (otherCol in 0..2) {
-                if (col != otherCol) {
-                    if (getCol(cells, otherCol) == "XXX") {
-                        return "Impossible"
-                    }
-                }
-            }
-            return "O wins"
-        }
-    }
-    return null
-}
-
-fun checkDiagonals(cells: String): String? {
-    if (cells[0] == cells[4] && cells[4] == cells[8]) { // top left to bottom right
-        return "${cells[0]} wins"
-    }
-    if (cells[2] == cells[4] && cells[4] == cells[6]) { // top right to bottom left
-        return "${cells[2]} wins"
-    }
-    return null
-}
-
-fun checkState(cells: String) {
-    val countX = cells.filter { it == 'X' }.length
-    val countO = cells.filter { it == 'O' }.length
-    if (kotlin.math.abs(countO - countX) > 1) {
-        println("Impossible")
-        return
-    }
-
-    val rowResult = checkRows(cells)
-    if (rowResult != null) {
-        println(rowResult)
-        return
-    }
-
-    val colResult = checkColumns(cells)
-    if (colResult != null) {
-        println(colResult)
-        return
-    }
-
-    val diagResult = checkDiagonals(cells)
-    if (diagResult != null) {
-        println(diagResult)
-        return
-    }
-
-    if (cells.contains("_")) {
-        println("Game not finished")
-    } else {
-        println("Draw")
-    }
-}
+import kotlin.math.abs
 
 fun main() {
-    print("Enter cells:")
-    val cells = readLine()!!
+    print("Enter cells: ")
+    val input = readln().take(9)
+    val cells = input.toList().windowed(size = 3, step = 3)
+    cells[0][0]
 
     printBoard(cells)
+    println(getGameState(cells))
+}
 
-    checkState(cells)
+
+fun printBoard(cells: List<List<Char>>) {
+    val outer = "-".repeat(9)
+    val rows = cells.map { it.joinToString(separator = " ", prefix = "| ", postfix = " |") }
+
+    println(outer)
+    rows.forEach(::println)
+    println(outer)
+}
+
+fun getGameState(cells: List<List<Char>>): String {
+    val symbols = cells.flatten().groupingBy { it }.eachCount().withDefault { 0 }
+    val xCount = symbols.getValue('X')
+    val oCount = symbols.getValue('O')
+    val emptyCount = symbols.getValue('_')
+
+    if ( abs(xCount - oCount) > 1) {
+        return "Impossible"
+    }
+
+    val lines = mutableListOf<List<Char>>()
+    lines += cells
+    lines += (0..2).map { i -> cells.map { row -> row[i] } }
+    lines += (0..2).map { i -> cells[i][i] }
+    lines += (0..2).map { i -> cells[i][cells.lastIndex - i] }
+
+    val wins = lines.map(List<Char>::toSet).filter { it.size == 1 && ('_' !in it) }.flatten()
+
+    return when(wins.size) {
+        0 -> if(emptyCount > 0) "Game not finished" else "Draw"
+        1 -> "${wins.first()} wins"
+        else -> "Impossible"
+    }
 }
